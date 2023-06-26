e9cb5f0bb67b48c38f6893aca05a6b68f8036120
AlexisEV <101740078+AlexisEV@users.noreply.github.com>
2023-06-25 21:44:51 -0500

    Add files via upload

---
+++ Modelo/Mpokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedexFinal.Modelo
+{
+    public class Mpokemon
+    {
+        public string Colorfondo { get; set; }
+        public string Imagen { get; set; }
+        public string Nombre { get; set; }
+        public string Nropokedex { get; set; }
+        public string Tipo { get; set; }
+        public string Colortipo { get; set; }
+        public string Idpokemon { get; set; }
+    }
+}
