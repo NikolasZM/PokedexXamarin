7d6ad8599bf50119862e3fc30e0f830616dba1c6
AlexisEV <101740078+AlexisEV@users.noreply.github.com>
2023-06-25 21:30:32 -0500

    Add files via upload

---
+++ Datos/Dpokemon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedexFinal.Datos
+{
+    internal class Dpokemon
+    {
+    }
+}
---
+++ Modelo/Mpokemon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedexFinal.Modelo
+{
+    internal class Mpokemon
+    {
+    }
+}
